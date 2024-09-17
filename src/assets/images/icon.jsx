8b662ca7e1aca3bb5f3058c66756286242f3f0c7
F1rdavs5767 <firdavsistamow@gmail.com>
2024-09-17 22:35:52 +0500

    Update

--- src/assets/images/icon.jsx
+++ src/assets/images/icon.jsx
@@ -338,10 +338,10 @@ function ClockIcon() {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g clip-path="url(#clip0_5491_1439)">
+      <g clipPath="url(#clip0_5491_1439)">
         <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          fillRule="evenodd"
+          clipRule="evenodd"
           d="M23 14C23 18.9706 18.9706 23 14 23C9.02944 23 5 18.9706 5 14C5 9.02944 9.02944 5 14 5C18.9706 5 23 9.02944 23 14ZM25 14C25 20.0751 20.0751 25 14 25C7.92487 25 3 20.0751 3 14C3 7.92487 7.92487 3 14 3C20.0751 3 25 7.92487 25 14ZM14.5 8.5H12.5V15.5H18V13.5H14.5V8.5Z"
           fill="#B3B3B3"
         />
